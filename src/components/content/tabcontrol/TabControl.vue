d5f6a3d6ac7cf47eb790ca87e5971a0651aeff4f
武人 <49179402+Mr-wr@users.noreply.github.com>
2020-12-13 17:25:37 +0800

    添加了TabControl流行控制

---
+++ src/components/content/tabcontrol/TabControl.vue
@@ -0,0 +1,65 @@
+<!--
+ * @Author: qi-you
+ * @Date: 2020-12-13 13:45:32
+ * @LastEditors: qi-you
+ * @LastEditTime: 2020-12-13 17:19:32
+ * @Descripttion: 
+-->
+<template>
+  <div class="tab-control">
+    <div
+      class="tab-item"
+      v-for="(item, index) in itemArray"
+      :key="index"
+      :class="{ active: currentIndex == index }"
+      @click="itemClick(index)"
+    >
+      <span>
+        {{ item }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemArray: {
+      type: Array,
+      defualt() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  position: sticky;
+  top: 44px;
+  background-color:#fff;
+  text-align: center;
+  font-size: 14px;
+}
+.tab-item {
+  flex: 1;
+  height: 40px;
+  line-height: 40px;
+}
+.active {
+  color: var(--color-high-text);
+  border-bottom: 2px solid var(--color-tint);
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,7 +2,7 @@
  * @Author: qi-you
  * @Date: 2020-12-01 17:44:19
  * @LastEditors: qi-you
- * @LastEditTime: 2020-12-11 21:32:53
+ * @LastEditTime: 2020-12-13 17:05:39
  * @Descripttion: 
 -->
 <template>
@@ -13,6 +13,107 @@
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend :recommend="recommends"></home-recommend>
     <home-feature></home-feature>
+    <tab-control :itemArray="['流行','新款','精选']"></tab-control>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
   </div>
 </template>
 <script>
@@ -20,13 +121,20 @@ import NavBar from "common/navbar/NavBar";
 import HomeSwiper from "views/home/childcomps/HomeSwiper";
 import { getHomeMultidata } from "network/home";
 import HomeRecommend from "views/home/childcomps/HomeRecommend";
-import HomeFeature from 'views/home/childcomps/HomeFeature';
+import HomeFeature from "views/home/childcomps/HomeFeature";
+import TabControl from "../../components/content/tabcontrol/TabControl";
 export default {
   name: "Home",
   data() {
     return { banners: [], recommends: [] };
   },
-  components: { NavBar, HomeSwiper, HomeRecommend,HomeFeature },
+  components: {
+    NavBar,
+    HomeSwiper,
+    HomeRecommend,
+    HomeFeature,
+    TabControl,
+  },
   created() {
     getHomeMultidata()
       .then((res) => {
@@ -43,17 +151,16 @@ export default {
 </script>
 
 <style>
-.home{
-  padding-top:44px;
+.home {
+  padding-top: 44px;
 }
 .home-bar {
-  position:fixed;
-  top:0;
-  left:0;
+  position: fixed;
+  top: 0;
+  left: 0;
   right: 0;
   background-color: var(--color-tint);
   z-index: 1;
-
 }
 .home-font {
   color: #fff;
