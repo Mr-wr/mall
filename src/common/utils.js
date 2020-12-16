e552d50350cdfbd08a238fea14b1ea7f1558140c
武人 <49179402+Mr-wr@users.noreply.github.com>
2020-12-16 20:24:17 +0800

    添加debounce函数说明

--- src/common/utils.js
+++ src/common/utils.js
@@ -2,10 +2,16 @@
  * @Author: qi-you
  * @Date: 2020-12-16 14:41:25
  * @LastEditors: qi-you
- * @LastEditTime: 2020-12-16 14:42:05
+ * @LastEditTime: 2020-12-16 14:54:43
  * @Descripttion:
  */
 // 封装放抖动
+/**
+ * @description: 防抖动
+ * @param {funciton} func 要防抖动的方法
+ * @param {String} delay 要防抖动毫秒数
+ * @return {funciton} 返回一个方法
+ */
 export function debounce(func, delay) {
   let time = null;
   return (...arg) => {
