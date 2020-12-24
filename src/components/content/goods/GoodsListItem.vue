5d54cf8e38c525b9260b3c08666d0e5549915ad5
武人 <49179402+Mr-wr@users.noreply.github.com>
2020-12-24 17:18:37 +0800

    添加点击事件-点击跳转到详情界面

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -2,11 +2,11 @@
  * @Author: qi-you
  * @Date: 2020-12-14 17:26:01
  * @LastEditors: qi-you
- * @LastEditTime: 2020-12-16 11:01:33
+ * @LastEditTime: 2020-12-24 16:53:54
  * @Descripttion: 
 -->
 <template>
-  <div class="goods-item">
+  <div class="goods-item" @click="todetail">
     <img :src="goodsItem.show.img" alt="" @load="itemImageLoad" />
     <div class="goods-info">
       <p>{{ goodsItem.title }}</p>
@@ -31,6 +31,9 @@ export default {
     itemImageLoad() {
       // 事件总线发射实现
       this.$bus.$emit("itemImageLoad");
+    },
+    todetail() {
+      this.$router.push("/detail/" + this.goodsItem.iid);
     },
   },
 };
