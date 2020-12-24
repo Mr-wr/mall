577d0f68d9b4717f39ee1238e96ac1c20a45008f
武人 <49179402+Mr-wr@users.noreply.github.com>
2020-12-24 17:16:45 +0800

    添加前置守卫-利用守卫给title复制

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ const Home = () => import("views/home/Home");
 const Cart = () => import("views/cart/Cart");
 const Category = () => import("views/category/Category");
 const Profile = () => import("views/profile/Profile");
+const Detail = () => import("views/detail/Detail");
 const routes = [
   {
     path: "",
@@ -14,19 +15,38 @@ const routes = [
   },
   {
     path: "/home",
+    meta: {
+      title: "首页"
+    },
     component: Home
   },
   {
     path: "/cart",
+    meta: {
+      title: "购物车"
+    },
     component: Cart
   },
   {
     path: "/category",
+    meta: {
+      title: "分类菜单"
+    },
     component: Category
   },
   {
     path: "/profile",
+    meta: {
+      title: "我的"
+    },
     component: Profile
+  },
+  {
+    path: "/detail/:iid",
+    meta: {
+      title: "商品详情"
+    },
+    component: Detail
   }
 ];
 
@@ -34,6 +54,11 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
