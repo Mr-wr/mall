ea1a3cfce621c33de3c3be0738bf0f7c06a928e2
武人 <49179402+Mr-wr@users.noreply.github.com>
2020-12-28 22:30:52 +0800

    detail添加scroll可以滚动区

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="detail">
+    <detail-title-bar class="title-bar" />
     <scroll class="detail-scroll" ref="detail-scroll">
-      <detail-title-bar />
       <child-swiper :topImages="topImages"></child-swiper>
       <child-base-info :goods="goods"></child-base-info>
+      <child-shop-info></child-shop-info>
     </scroll>
   </div>
 </template>
@@ -11,10 +12,11 @@
 import DetailTitleBar from "./childComps/DetailTitleBar";
 import ChildSwiper from "./childComps/childSwiper";
 import childBaseInfo from "./childComps/childBaseInfo";
+import childShopInfo from "./childComps/childShopInfo";
 
 import Scroll from "common/scroll/Scroll";
 
-import { getDetail, Goods } from "network/detail";
+import { getDetail, Goods, Shop } from "network/detail";
 export default {
   name: "Detail",
   components: {
@@ -22,6 +24,7 @@ export default {
     ChildSwiper,
     Scroll,
     childBaseInfo,
+    childShopInfo,
   },
   data() {
     return {
@@ -34,18 +37,34 @@ export default {
         default() {
           return {};
         },
+        shop: {
+          type: Object,
+          default() {
+            return {};
+          },
+        },
       },
     };
   },
   methods: {},
   created() {
+    // 1.保存传入的iid
     this.iid = this.$route.params.iid;
+    // 2.根据iid获取数据
     getDetail(this.iid).then((res) => {
-      const data = res;
-      this.topImages = data.result.itemInfo.topImages;
+      const data = res.result;
+      // 3.获取轮播图数据
+      this.topImages = data.itemInfo.topImages;
       // console.log(data);
-      this.goods = new Goods(data.result.itemInfo, data.result.columns, data.result.shopInfo.services);
+      // 获取商品基本信息
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
       // console.log(this.goods);
+      // 创建店铺信息对象
+      this.shop = new Shop(data.shopInfo);
     });
   },
   mounted() {},
@@ -54,6 +73,17 @@ export default {
 
 <style scoped>
 .detail {
+  position: relative;
   background-color: #f0f0f0;
+  z-index: 9;
+  height: 100vh;
+}
+.title-bar{
+  position: relative;
+  background-color: #f0f0f0;
+  z-index: 9;
+}
+.detail-scroll {
+  height: calc(100% - 44px);
 }
 </style>
