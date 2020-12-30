d3a7fec56d09e3f22b8972dd1af055900178a057
武人 <49179402+Mr-wr@users.noreply.github.com>
2020-12-30 11:02:48 +0800

    detail-添加商品类

--- src/network/detail.js
+++ src/network/detail.js
@@ -2,7 +2,7 @@
  * @Author: qi-you
  * @Date: 2020-12-24 17:07:30
  * @LastEditors: qi-you
- * @LastEditTime: 2020-12-28 16:30:49
+ * @LastEditTime: 2020-12-28 21:57:08
  * @Descripttion:
  */
 import { request } from "./request";
@@ -24,5 +24,15 @@ export class Goods {
     this.columns = columns;
     this.services = services;
     this.realPrice = itemInfo.lowNowPrice;
+  }
+}
+export class Shop {
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo;
+    this.name = shopInfo.name;
+    this.fans = shopInfo.cFans;
+    this.sells = shopInfo.cSells;
+    this.score = shopInfo.score;
+    this.goodsCount = shopInfo.cGoods;
   }
 }
