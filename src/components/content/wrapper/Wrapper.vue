65f31ddfcfbc4fdeaf764ef842979e5a7e75f2ce
武人 <49179402+Mr-wr@users.noreply.github.com>
2020-12-30 11:00:22 +0800

    wrapper-添加详情包裹组件

---
+++ src/components/content/wrapper/Wrapper.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: qi-you
+ * @Date: 2020-12-29 18:05:16
+ * @LastEditors: qi-you
+ * @LastEditTime: 2020-12-30 08:36:41
+ * @FilePath: \spuer_mall\src\components\content\wrapper\Wrapper.vue
+ * @Description: 
+-->
+<template>
+  <div class="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  color: #000;
+}
+.wrapper > .content {
+  margin-top: 10px;
+  padding: 10px;
+  width: 95%;
+  border-radius: 10px;
+  background-color: #fff;
+}
+</style>
